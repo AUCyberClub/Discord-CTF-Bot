bffcc467a9a7964c6f451d1854f33b17096aa191
ebubekirtrkr <ebubekirtrkr@gmail.com>
2022-02-15 16:13:52 +0300

    actions added and config updated

--- config.py
+++ config.py
@@ -1,16 +1,27 @@
-TOKEN                 = "<TOKEN>"               #string
-COMMAND_PREFIX        = "<COMMAND_PREFIX>"      #string
-SCOREBOARD_CHANNEL_ID = <SCOREBOARD_CHANNEL_ID> #integer
-CHALLENGES_CHANNEL_ID = <CHALLENGES_CHANNEL_ID> #integer
-DISCUSSION_CHANNEL_ID = <DISCUSSION_CHANNEL_ID> #integer
-LOG_CHANNEL_ID        = <LOG_CHANNEL_ID>        #integer
-ADMIN_CHANNEL_ID      = <ADMIN_CHANNEL_ID>      #integer
+import os
+from dotenv import load_dotenv
+#for local tests
+try:
+    load_dotenv('.env')
+except:
+    pass
 
-HOST                  = "<HOST>"                #string
-USERNAME              = "<USERNAME>"            #string
-PASSWORD              = "<PASSWORD>"            #string
-PORT                  = <PORT>                  #integer
-DATABASE              = "<DATABASE>"            #string
+#DISCORD RELATED
+TOKEN                 = os.getenv("DISCORD_TOKEN")                       #string
+COMMAND_PREFIX        = os.getenv("DISCORD_COMMAND_PREFIX")              #string
+SCOREBOARD_CHANNEL_ID = int(os.getenv("DISCORD_SCOREBOARD_CHANNEL_ID"))  #integer
+CHALLENGES_CHANNEL_ID = int(os.getenv("DISCORD_CHALLENGES_CHANNEL_ID"))  #integer
+DISCUSSION_CHANNEL_ID = int(os.getenv("DISCORD_DISCUSSION_CHANNEL_ID"))  #integer
+LOG_CHANNEL_ID        = int(os.getenv("DISCORD_LOG_CHANNEL_ID"))         #integer
+ADMIN_CHANNEL_ID      = int(os.getenv("DISCORD_ADMIN_CHANNEL_ID"))       #integer
+DATABASE_CHANNEL_ID   = int(os.getenv("DISCORD_DATABASE_CHANNEL_ID"))    #integer
 
+#DATABASE RELATED
+HOST                  = os.getenv("DATABASE_HOST")                       #string
+USERNAME              = os.getenv("DATABASE_USERNAME")                   #string
+PASSWORD              = os.getenv("DATABASE_PASSWORD")                   #string
+PORT                  = int(os.getenv("DATABASE_PORT"))                  #integer
+DATABASE              = os.getenv("DATABASE_DATABASE")                   #string
 
-LANGUAGE              ="<BOT_LANGUAGE>"         #string, ex "tr","en"
+#BOT LANGUAGE
+LANGUAGE              = os.getenv("BOT_LANGUAGE")                        #string, ex "tr","en"
